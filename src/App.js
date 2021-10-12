4b38fcf81f11d39613ae6ef3d4284d8eb229273f
jyst <yeongsinjoung@gmail.com>
2021-10-12 10:41:22 +0900

    change portfolio to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import './index.css';
+import React, {Component} from "react";
+import MenuBtn from "./components/MenuBtn"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Project from "./components/Project"
+import Contact from "./components/Contact"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MenuBtn></MenuBtn>
+        <Header></Header>
+        <main>
+          <Hero></Hero>
+          <About></About>
+          <Project></Project>
+          <Contact></Contact>
+        </main>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 
 export default App;
